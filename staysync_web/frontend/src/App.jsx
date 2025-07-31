4690ffeb8bcdce0d00fa66669bd28f8d16bd80b8
Sarang <sarangsanthoshkumar@gmail.com>
2025-07-31 10:08:23 +0000

    Frontend_Web

---
+++ staysync_web/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Verify from './pages/Verify';
+
+const App = () => {
+  return (
+    <div>
+      <h1></h1>
+      <Verify />
+    </div>
+  );
+};
+
+export default App;
